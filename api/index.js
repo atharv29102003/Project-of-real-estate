09a9ce117239e0d78d07f2694250673d8474004f
atharv29102003 <atharv.v.deshpande@gmail.com>
2024-06-01 20:25:36 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("MongoDB connection string:", process.env.MONGO);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 app.listen(3000, () => {
